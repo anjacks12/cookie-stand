317018fcd4e94fe037eeeba9086c1c26492942f5
Angela Jackson <anjacks12@gmail.com>
2021-12-30 21:51:25 -0800

    completed lab 07

--- js/app.js
+++ js/app.js
@@ -125,9 +125,14 @@ Store.prototype.renderHourlySales = function() {
   table.appendChild(tfoot);
   let trFoot = document.createElement('tr');
   tfoot.appendChild(trFoot);
-  let hourlyTotal = document.createElement('td');
-  hourlyTotal.textContent = 'something here';
-  trFoot.appendChild(hourlyTotal);
+  let tdEmpty = document.createElement('td');
+  tdEmpty.textContent = 'Hourly Total';
+  trFoot.appendChild(tdEmpty);
+  for (let i =0; i < hoursArray.length; i++) {
+    let hourlyTotal = document.createElement('td');
+    hourlyTotal.textContent = totalHourlyCookiesSaleArray[i];
+    trFoot.appendChild(hourlyTotal);
+  }
 };
 // seattle.renderCookieSales();
 // tokyo.renderCookieSales();
@@ -136,22 +141,92 @@ Store.prototype.renderHourlySales = function() {
 // lima.renderCookieSales();
 
 // to get store hours to show up in table
-seattle.renderHours();
 
-let hourlyArray = [];
-
+// instantiating(?) all 5 stores and rendering table below
 for (let i = 0; i < storeDataArray.length; i++) {
   storeDataArray[i].render();
   storeDataArray[i].renderCookieSales();
-  hourlyArray.push(storeDataArray[i].cookiesSoldArray[0]);
-  let hour1 = hourlyArray[0] + hourlyArray[1] + hourlyArray[2] + hourlyArray [3] + hourlyArray [4];
-  //console.log(storeDataArray[i].cookiesSoldArray[i]);
-  console.log(hour1);
 }
 
+for (let i = 4; i < storeDataArray.length; i++) {
+  storeDataArray[i].renderHours();
+}
+
+let hourlyCookiesArray = [];
+
+for (let i = 0; i < storeDataArray.length; i++) {
+  for (let a = 0; a < hoursArray.length; a++) {
+    //console.log(storeDataArray[i].cookiesSoldArray[a]);
+    let hourOne = storeDataArray[i].cookiesSoldArray[a];
+    //console.log(hourOne);
+    hourlyCookiesArray.push(hourOne);
+  }
+}
+
+let totalHourlyCookiesSaleArray = [];
+
+let hour01 = hourlyCookiesArray[0] + hourlyCookiesArray[14] + hourlyCookiesArray[28] + hourlyCookiesArray[42] + hourlyCookiesArray[56];
+//console.log(hour01);
+totalHourlyCookiesSaleArray.push(hour01);
+
+let hour02 = hourlyCookiesArray[1] + hourlyCookiesArray[15] + hourlyCookiesArray[29] + hourlyCookiesArray[43] + hourlyCookiesArray[57];
+//console.log(hour02);
+totalHourlyCookiesSaleArray.push(hour02);
+
+let hour03 = hourlyCookiesArray[2] + hourlyCookiesArray[16] + hourlyCookiesArray[30] + hourlyCookiesArray[44] + hourlyCookiesArray[58];
+//console.log(hour03);
+totalHourlyCookiesSaleArray.push(hour03);
+
+let hour04 = hourlyCookiesArray[3] + hourlyCookiesArray[17] + hourlyCookiesArray[31] + hourlyCookiesArray[45] + hourlyCookiesArray[59];
+//console.log(hour04);
+totalHourlyCookiesSaleArray.push(hour04);
+
+let hour05 = hourlyCookiesArray[4] + hourlyCookiesArray[18] + hourlyCookiesArray[32] + hourlyCookiesArray[46] + hourlyCookiesArray[60];
+//console.log(hour05);
+totalHourlyCookiesSaleArray.push(hour05);
+
+let hour06 = hourlyCookiesArray[5] + hourlyCookiesArray[19] + hourlyCookiesArray[33] + hourlyCookiesArray[47] + hourlyCookiesArray[61];
+//console.log(hour06);
+totalHourlyCookiesSaleArray.push(hour06);
+
+let hour07 = hourlyCookiesArray[6] + hourlyCookiesArray[20] + hourlyCookiesArray[34] + hourlyCookiesArray[48] + hourlyCookiesArray[62];
+//console.log(hour07);
+totalHourlyCookiesSaleArray.push(hour07);
+
+let hour08 = hourlyCookiesArray[7] + hourlyCookiesArray[21] + hourlyCookiesArray[35] + hourlyCookiesArray[49] + hourlyCookiesArray[63];
+//console.log(hour08);
+totalHourlyCookiesSaleArray.push(hour08);
+
+let hour09 = hourlyCookiesArray[8] + hourlyCookiesArray[22] + hourlyCookiesArray[36] + hourlyCookiesArray[50] + hourlyCookiesArray[64];
+//console.log(hour09);
+totalHourlyCookiesSaleArray.push(hour09);
+
+let hour10 = hourlyCookiesArray[9] + hourlyCookiesArray[23] + hourlyCookiesArray[37] + hourlyCookiesArray[51] + hourlyCookiesArray[65];
+//console.log(hour10);
+totalHourlyCookiesSaleArray.push(hour10);
+
+let hour11 = hourlyCookiesArray[10] + hourlyCookiesArray[24] + hourlyCookiesArray[38] + hourlyCookiesArray[52] + hourlyCookiesArray[66];
+//console.log(hour11);
+totalHourlyCookiesSaleArray.push(hour11);
+
+let hour12 = hourlyCookiesArray[11] + hourlyCookiesArray[25] + hourlyCookiesArray[39] + hourlyCookiesArray[53] + hourlyCookiesArray[67];
+//console.log(hour12);
+totalHourlyCookiesSaleArray.push(hour12);
+
+let hour13 = hourlyCookiesArray[12] + hourlyCookiesArray[26] + hourlyCookiesArray[40] + hourlyCookiesArray[54] + hourlyCookiesArray[68];
+//console.log(hour13);
+totalHourlyCookiesSaleArray.push(hour13);
+
+let hour14 = hourlyCookiesArray[13] + hourlyCookiesArray[27] + hourlyCookiesArray[41] + hourlyCookiesArray[55] + hourlyCookiesArray[69];
+//console.log(hour14);
+totalHourlyCookiesSaleArray.push(hour14);
+
+//console.log(totalHourlyCookiesSaleArray);
 
 
-
+for (let i = 4; i < storeDataArray.length; i++) {
+  storeDataArray[i].renderHourlySales();
+}
 
 
 
